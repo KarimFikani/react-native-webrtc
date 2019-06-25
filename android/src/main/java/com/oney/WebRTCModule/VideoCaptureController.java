9b7679108f2393adfac8d32c194a4d42194964e9
Hao Wu <hwu@atheerair.com>
2019-06-24 18:13:00 -0700

    hao2

--- android/src/main/java/com/oney/WebRTCModule/VideoCaptureController.java
+++ android/src/main/java/com/oney/WebRTCModule/VideoCaptureController.java
@@ -14,6 +14,7 @@ import org.webrtc.Camera2Enumerator;
 import org.webrtc.CameraEnumerator;
 import org.webrtc.CameraVideoCapturer;
 import org.webrtc.VideoCapturer;
+import org.webrtc.CameraVideoCapturer;
 
 import java.util.ArrayList;
 import java.util.List;
